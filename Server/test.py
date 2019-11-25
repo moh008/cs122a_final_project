0bb4cfaf29964662cb0979ab9469353dd764c611
moh008 <moh008@ucr.edu>
2019-11-24 18:34:35 -0800

    Radar scanning module updated

--- Server/test.py
+++ Server/test.py
@@ -19,8 +19,7 @@ def test_Led():
         led.colorWipe(led.strip, Color(0,0,0))  #turn off the light
         print "\nEnd of program"
 
-        
-        
+
 from Motor import *            
 PWM=Motor()          
 def test_Motor(): 
@@ -91,7 +90,102 @@ def test_Servo():
         pwm.setServoPwm('0',90)
         pwm.setServoPwm('1',90)
         print "\nEnd of program"
+
+
+from Led import *
+from servo import *
+from Ultrasonic import *
+from Buzzer import *
+buzzer = Buzzer()
+ultrasonic = Ultrasonic()
+pwm = Servo()
+led=Led()
+
+def test_nodrill():
+    try:
+        print "\nScanning surrounding Area..."
+        pwm.setServoPwm('1', 120)
+        time.sleep(3)
+        while True:
+            data = ultrasonic.get_distance()
+            print ("\n Distance: " + str(data) + "cm")
+            if data > 30:
+                for i in range(0, 200, 4):
+                    data = ultrasonic.get_distance()
+                    print ("\n Distance: " + str(data) + "cm")
+                    time.sleep(0.01)
+                    if data > 30:
+                        pwm.setServoPwm('0', i)
+                        #time.sleep(0.01)
+                    else:
+                        break
+                for i in range(200, 0, -4):
+                    data = ultrasonic.get_distance()
+                    print ("\n Distance: " + str(data) + "cm")
+                    time.sleep(0.01)
+                    if data > 30:
+                        pwm.setServoPwm('0', i)
+                        #time.sleep(0.01)
+                    else:
+                        break
+            else:
+                print "\nHostile AC-130 Above"
+                buzzer.run('1')
+                led.ledIndex(0x01,255,0,0)      #buzzer on turn Red led
+                led.ledIndex(0x02,255,0,0)
+                led.ledIndex(0x04,255,0,0)
+                led.ledIndex(0x08,255,0,0)
+                led.ledIndex(0x10,255,0,0)
+                led.ledIndex(0x20,255,0,0)
+                led.ledIndex(0x40,255,0,0)
+                led.ledIndex(0x80,255,0,0)
+                time.sleep(1)                   #wait for three seconds
+                buzzer.run('0')
+                
+                time.sleep(1)                   #silence interval
+                
+                buzzer.run('1')
+                time.sleep(1)
+                buzzer.run('0')
+                
+                time.sleep(1)
+                buzzer.run('1')
+                
+                time.sleep(1)
+                buzzer.run('0')
+                
+                led.colorWipe(led.strip, Color(0,0,0))  #turn off all the light
+                print "\n\n\nFriendly AA tank deployed"
+                pwm.setServoPwm('0',90)
+                pwm.setServoPwm('1',90)
+                break
+            """
+    except data < 10:            
+        print "\nHostile AC-130 Above"
+        buzzer.run('1')
+        led.ledIndex(0x01,255,0,0)      #buzzer on turn Red led
+        led.ledIndex(0x02,255,0,0)
+        led.ledIndex(0x04,255,0,0)
+        led.ledIndex(0x08,255,0,0)
+        led.ledIndex(0x10,255,0,0)
+        led.ledIndex(0x20,255,0,0)
+        led.ledIndex(0x40,255,0,0)
+        led.ledIndex(0x80,255,0,0)
+        time.sleep(1)                   #wait for three seconds
+        buzzer.run('0') and led.colorWipe(led.strip, Color(0,0,0))  #turn off all the light
+        print "\n\n\nFriendly AA tank deployed"
+        pwm.setServoPwm('0',90)
+        pwm.setServoPwm('1',90)
+        """
+    except KeyboardInterrupt:
+        led.colorWipe(led.strip, Color(0,0,0))  #turn off the light
+        pwm.setServoPwm('0',90)
+        pwm.setServoPwm('1',90)
+        buzzer.run('0')
         
+        print "\nEnd of program"
+
+
         
 from ADC import *
 adc=Adc()
@@ -126,6 +220,7 @@ def test_Buzzer():
         buzzer.run('0')
         print "\nEnd of program"
         
+      
 import cv2
 def test_Camera():
         try:
@@ -164,7 +259,9 @@ if __name__ == '__main__':
     elif sys.argv[1] == 'Buzzer':   
         test_Buzzer() 
     elif sys.argv[1] == 'Camera':  
-        test_Camera() 
+        test_Camera()
+    elif sys.argv[1] == 'nodrill':
+        test_nodrill()
 
         
         
